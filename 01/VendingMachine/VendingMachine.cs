0d0522cdf35245240dc9107805dcb28d963ab4e1
klangley19 <klangley@tm.net>
2016-11-16 00:35:41 -0500

    Added the method to vending machine that will take the size and weight for a coin and determine if the coin is valid and then get the value for the coin and add it to the running value total of the coins in the machine

--- 01/VendingMachine/VendingMachine.cs
+++ 01/VendingMachine/VendingMachine.cs
@@ -8,10 +8,36 @@ namespace VendingMachine
 {
     public class VendingMachine
     {
+        private int ValueInMachine;
+
+        public bool DepositCoin(int CoinSize, int CoinWeight)
+        {
+            if (Coin.IsValidCoin(CoinSize, CoinWeight))
+            {
+                if (CoinSize == Coin.GetSizeForNickel())
+                {
+                    ValueInMachine += Coin.GetValueForNickel();
+                    return true;
+                }
+                else if (CoinSize == Coin.GetSizeForDime())
+                {
+                    ValueInMachine += Coin.GetValueForDime();
+                    return true;
+                }
+                else if (CoinSize == Coin.GetSizeForQuarter())
+                {
+                    ValueInMachine += Coin.GetValueForQuarter();
+                    return true;
+                }
+                return false;
+            }
+            else
+            {
+                //code here to send the invalid coin back in the coin return of the machine
+                return false;
+            }
 
 
-
-
-
+        }
     }
 }
