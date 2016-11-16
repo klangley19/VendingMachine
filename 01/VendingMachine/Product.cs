97698ea33314b10f7e789a0f32c6b570db3c9ba6
klangley19 <klangley@tm.net>
2016-11-16 08:25:43 -0500

    Added methods for getting the cost for the items from the Product class and fixed a variable name issue in the unit test for the product.

---
+++ 01/VendingMachine/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    public static class Product
+    {
+        public static int GetCostForACola()
+        {
+            return 100;
+        }
+
+        public static int GetCostForACandy()
+        {
+            return 65;
+        }
+
+        public static int GetCostForABagOfChips()
+        {
+            return 50;
+        }
+
+
+    }
+}
--- 01/VendingMachineUnitTests/SelectProductUnitTests.cs
+++ 01/VendingMachineUnitTests/SelectProductUnitTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using VendingMachine;
+
 namespace VendingMachineUnitTests
 {
     [TestClass]
@@ -10,21 +12,21 @@ namespace VendingMachineUnitTests
         public void Get_The_Cost_For_A_Cola_Is_Not_One_Dollar()
         {
             var productCost = Product.GetCostForACola();
-            Assert.AreEqual(100, coinValue);
+            Assert.AreEqual(100, productCost);
         }
 
         [TestMethod]
         public void Get_The_Cost_For_A_Candy_Is_Not_Sixty_Five_Cents()
         {
             var productCost = Product.GetCostForACandy();
-            Assert.AreEqual(65, coinValue);
+            Assert.AreEqual(65, productCost);
         }
 
         [TestMethod]
         public void Get_The_Cost_For_A_Bag_Of_Chips_Is_Not_FIfty_Cents()
         {
             var productCost = Product.GetCostForABagOfChips();
-            Assert.AreEqual(50, coinValue);
+            Assert.AreEqual(50, productCost);
         }
 
     }
