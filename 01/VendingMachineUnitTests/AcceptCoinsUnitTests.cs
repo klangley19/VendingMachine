385736b9c0eb0e109d82bc1852fda818df00a638
klangley19 <klangley@tm.net>
2016-11-15 22:17:28 -0500

    Adding six unit tests for checking the weight and size for nickels, dimes and quarters

--- 01/VendingMachineUnitTests/AcceptCoinsUnitTests.cs
+++ 01/VendingMachineUnitTests/AcceptCoinsUnitTests.cs
@@ -10,25 +10,70 @@ namespace VendingMachineUnitTests
     {
 
         [TestMethod]
-        public void A_Coin_Does_Not_Have_A_Nickel_Value_Throws_Invalid_Operation_Exception()
+        public void A_Coin_Does_Not_Have_A_Nickel_Value()
         {
             var coinValue = Coin.GetValueForNickel();
             Assert.AreEqual(5, coinValue);
         }
 
         [TestMethod]
-        public void A_Coin_Does_Not_Have_A_Dime_Value_Throws_Invalid_Operation_Exception()
+        public void A_Coin_Does_Not_Have_A_Dime_Value()
         {
             var coinValue = Coin.GetValueForDime();
             Assert.AreEqual(10, coinValue);
         }
 
         [TestMethod]
-        public void A_Coin_Does_Not_Have_A_Quarter_Value_Throws_Invalid_Operation_Exception()
+        public void A_Coin_Does_Not_Have_A_Quarter_Value()
         {
             var coinValue = Coin.GetValueForQuarter();
             Assert.AreEqual(25, coinValue);
         }
+
+
+        [TestMethod]
+        public void A_Coin_Does_Not_Have_A_Nickel_Weight()
+        {
+            var coinValue = Coin.GetWeightForNickel();
+            Assert.AreEqual(4, coinValue);
+        }
+
+        [TestMethod]
+        public void A_Coin_Does_Not_Have_A_Dime_Weight()
+        {
+            var coinValue = Coin.GetWeightForDime();
+            Assert.AreEqual(2, coinValue);
+        }
+
+        [TestMethod]
+        public void A_Coin_Does_Not_Have_A_Quarter_Weight()
+        {
+            var coinValue = Coin.GetWeightForQuarter();
+            Assert.AreEqual(5, coinValue);
+        }
+
+
+        [TestMethod]
+        public void A_Coin_Does_Not_Have_A_Nickel_Size()
+        {
+            var coinValue = Coin.GetSizeForNickel();
+            Assert.AreEqual(4, coinValue);
+        }
+
+        [TestMethod]
+        public void A_Coin_Does_Not_Have_A_Dime_Size()
+        {
+            var coinValue = Coin.GetSizeForDime();
+            Assert.AreEqual(2, coinValue);
+        }
+
+        [TestMethod]
+        public void A_Coin_Does_Not_Have_A_Quarter_Size()
+        {
+            var coinValue = Coin.GetSizeForQuarter();
+            Assert.AreEqual(5, coinValue);
+        }
+
 
 
 
