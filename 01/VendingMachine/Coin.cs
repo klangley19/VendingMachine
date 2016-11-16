5ed8534d38fd87efd649e13545fcff99b1f23d07
klangley19 <klangley@tm.net>
2016-11-15 22:26:50 -0500

    Added six methods for getting the size and weight for nickels, dimes and quarters

--- 01/VendingMachine/Coin.cs
+++ 01/VendingMachine/Coin.cs
@@ -6,6 +6,37 @@ using System.Threading.Tasks;
 
 namespace VendingMachine
 {
+
+    #region public enum CoinValue
+    public enum CoinValue
+    {
+        Undefined = 0,
+        Nickel = 5,
+        Dime = 10,
+        Quarter = 25
+    }
+    #endregion
+
+    #region public enum CoinWeight
+    public enum CoinWeight
+    {
+        Undefined = 0,
+        Nickel = 4,
+        Dime = 2,
+        Quarter = 5
+    }
+    #endregion
+
+    #region public enum CoinSize
+    public enum CoinSize
+    {
+        Undefined = 0,
+        Nickel = 4,
+        Dime = 2,
+        Quarter = 5
+    }
+    #endregion
+
     public static class Coin
     {
         #region public static int GetValueForNickel()
@@ -28,6 +59,51 @@ namespace VendingMachine
             return 25;
         }
         #endregion
+
+
+        #region public static int GetWeightForNickel()
+        public static int GetWeightForNickel()
+        {
+            return 4;
+        }
+        #endregion
+
+        #region public static int GetWeightForDime()
+        public static int GetWeightForDime()
+        {
+            return 2;
+        }
+        #endregion
+
+        #region public static int GetWeightForQuarter()
+        public static int GetWeightForQuarter()
+        {
+            return 5;
+        }
+        #endregion
+
+        #region public static int GetSizeForNickel()
+        public static int GetSizeForNickel()
+        {
+            return 4;
+        }
+        #endregion
+
+        #region public static int GetSizeForDime()
+        public static int GetSizeForDime()
+        {
+            return 2;
+        }
+        #endregion
+
+        #region public static int GetSizeForQuarter()
+        public static int GetSizeForQuarter()
+        {
+            return 5;
+        }
+        #endregion
+
+
 
     }
 }
